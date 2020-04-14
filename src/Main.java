224e558b5fe3010b330b592dc37e2b10f9eb1efa
aimee.pederson <aimee.pederson@bluestembrands.com>
2020-04-14 17:12:07 -0500

    quiz 1 answer

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        Scanner input = new Scanner(System.in);
+
+        String question = "What is your favorite color?";
+        String choiceOne = "Orange";
+        String choiceTwo = "Green";
+        String choiceThree = "Silver";
 
         String correctAnswer = choiceTwo;
 
-        // Write a print statement asking the question
-        // Write a print statement giving the answer choices
+        System.out.println(question);
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
 
         // Have the user input an answer
-        // Retrieve the user's input
+        String userInput = input.next();
 
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
+        // then the user is correct and we want to print out a congrats message to the
+        // user.
+        if (userInput.equals(correctAnswer)) {
+            System.out.println("You're right!");
+        }
 
         // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
+        // then the user is incorrect and we want to print out a message saying that the
+        // user is incorrect as well as what the correct choice was.
+        else {
+            System.out.println("Sorry, the correct answer is " + correctAnswer);
+        }
     }
 
 }
